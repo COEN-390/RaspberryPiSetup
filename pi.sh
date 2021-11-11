9d281c44487a9637500e74356b1c95cc00a3796c
Xaavian Ali <63985071+XaavianAli@users.noreply.github.com>
2021-11-11 16:55:11 -0500

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -5,7 +5,7 @@
 
 cd ~
 sudo apt update
-sudo apt install git python3-pip libwebp-dev libtiff5 libopencv-dev libatlas-base-bev libopenblas-dev -y
+sudo apt install git vim python3-pip libwebp-dev libtiff5 libopencv-dev libatlas-base-bev libopenblas-dev -y
 git clone https://github.com/COEN-390/RaspberryPiSetup && cd RaspberryPiSetup
 pyhton3 -m pip install torch-1.7.0a0-cp37-cp37m-linux_armv7l.whl
 python3 -m pip install torchvision-0.8.0a0+45f960c-cp37-cp37m-linux_armv7l.whl
