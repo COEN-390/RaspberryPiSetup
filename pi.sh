13cb62afe9568837160c5e767e67771493c7e8d7
Xaavian Ali <63985071+XaavianAli@users.noreply.github.com>
2021-11-11 16:49:08 -0500

    Update pi.sh

--- pi.sh
+++ pi.sh
@@ -10,6 +10,9 @@ git clone https://github.com/COEN-390/RaspberryPiSetup && cd RaspberryPiSetup
 pyhton3 -m pip install torch-1.7.0a0-cp37-cp37m-linux_armv7l.whl
 python3 -m pip install torchvision-0.8.0a0+45f960c-cp37-cp37m-linux_armv7l.whl
 cd .. && git clone https://github.com/COEN-390/YOLOv5-Lite && cd YOLOv5-Lite
+python3 -m pip install opencv-python-headless=4.4.0.40
+sed -i 's/torchvision>=0.8.1/#torchvision>=0.8.1/g' requirements.txt
+sed -i 's/opencv-python>=4.1.2/#opencv-python>=4.1.2/g' requirements.txt
 python3 -m pip install -r requirements.txt
 
 ###To start the software
